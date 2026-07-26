91dcaec542fdcc18e15e6eb8cda79919149e1805
Zenzen <68279309+UnnuUno@users.noreply.github.com>
2026-07-27 04:54:23 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -113,7 +113,15 @@ export default function App() {
 
   const handleLogout = () => { localStorage.removeItem('faucet_session'); setCurrentUser(null); setFormUsername(''); setFormPassword(''); };
   const showNotification = (msg, type = 'info') => { setNotification({ msg, type }); setTimeout(() => setNotification(null), 3000); };
-  const startAd = () => { setShowAd(true); setAdTimer(5); setAdFinished(false); };
+  const startAd = () => { 
+  // BUKA IKLAN DI TAB BARU
+  window.open("https://www.effectivecpmnetwork.com/cvfeybzes4?key=1135154a38f4208f69b6ba63b3dda816", "_blank");
+
+  // MULAI HITUNG MUNDUR DI WEB ANDA
+  setShowAd(true); 
+  setAdTimer(5); 
+  setAdFinished(false); 
+};
 
   useEffect(() => {
     let interval = null;
